54b7e129901b5a24bc20df52b608bb5a8cb35df1
Vishwajeet_singh <sayvishwajeet56@gmail.com>
2021-10-11 04:45:32 +0530

    Update config.js

--- src/globals/config.js
+++ src/globals/config.js
@@ -3,7 +3,7 @@ const api = {
   //localhost: 'http://18.191.154.248:3001/',
 
   // localhost: 'http://localhost:3001/',
-  localhost: 'https://rathorebackend.herokuapp.com/',
+  localhost: 'https://bachod.herokuapp.com/',
 };
 
 let apiBase = '';
